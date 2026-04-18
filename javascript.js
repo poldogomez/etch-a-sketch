7c8e6e0afd8021670cff1dc3e6358cd65f3b0d4b
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2026-04-18 16:24:20 -0500

    Move small-square attributes to CSS

--- javascript.js
+++ javascript.js
@@ -3,6 +3,6 @@ let gridSide = 16;
 const container = document.querySelector("#container");
 
 const square = document.createElement("div");
-   square.setAttribute("style", "width:10px; height:10px; background-color:lightgrey; border:solid 1px blue;");
+   square.classList.add("small-square");
 
 container.appendChild(square);
