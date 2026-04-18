37b726cfd0c8d0585eeb4e5d2106ee6dff8e56e2
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2026-04-18 17:07:27 -0500

    Add calculation to limit max width of grid

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
 let gridSide = 16;
 
 const container = document.querySelector("#container");
-
-const square = document.createElement("div");
-   square.classList.add("small-square");
-
-container.appendChild(square);
+    container.style.maxWidth = (10 * gridSide + gridSide - 1) + "px";
+for (let i = 0; i < gridSide; i++) {
+    for (let j = 0; j < gridSide; j++) {
+        const square = document.createElement("div");
+            square.classList.add("small-square");
+        container.appendChild(square);
+    };
+};
