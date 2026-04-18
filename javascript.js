4c3996bd3b6b67fbf4d2db12af569c6ed88551f5
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2026-04-18 16:07:02 -0500

    Code js to create one square

---
+++ javascript.js
@@ -0,0 +1,8 @@
+let gridSide = 16;
+
+const container = document.querySelector("#container");
+
+const square = document.createElement("div");
+   square.setAttribute("style", "width:10px; height:10px; background-color:lightgrey; border:solid 1px blue;");
+
+container.appendChild(square);
