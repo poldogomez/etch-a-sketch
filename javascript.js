6d450c7c758fd573739ba7682502fc6cce22a6f3
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2026-04-19 17:06:08 -0500

    Create unique id for each square.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,16 @@
 let gridSide = 16;
-let sideLength = 30;
+let sideLength = 10;
+let idArr = [];
 
 const container = document.querySelector("#container");
     container.style.maxWidth = (sideLength * gridSide + gridSide - 1) + "px";
 for (let i = 1; i <= gridSide; i++) {
     for (let j = 1; j <= gridSide; j++) {
+        //create unique id
+        idArr.push(i + "-" + j);
+        // create grid
         const square = document.createElement("div");
+            square.id = i + "-" + j;
             square.classList.add("small-square");
             square.style.width = sideLength + "px";
             square.style.height = sideLength + "px";
