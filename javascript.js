a33c3242e2fa9004bc9fb95af0fdbd8d02363614
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2026-04-19 16:56:07 -0500

    Set width and height through js variables.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,14 @@
 let gridSide = 16;
+let sideLength = 30;
 
 const container = document.querySelector("#container");
-    container.style.maxWidth = (10 * gridSide + gridSide - 1) + "px";
-for (let i = 0; i < gridSide; i++) {
-    for (let j = 0; j < gridSide; j++) {
+    container.style.maxWidth = (sideLength * gridSide + gridSide - 1) + "px";
+for (let i = 1; i <= gridSide; i++) {
+    for (let j = 1; j <= gridSide; j++) {
         const square = document.createElement("div");
             square.classList.add("small-square");
+            square.style.width = sideLength + "px";
+            square.style.height = sideLength + "px";
         container.appendChild(square);
     };
 };
