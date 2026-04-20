eb238a11cdb494509cf3317cb8336e42458bf45a
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2026-04-20 10:40:53 -0500

    Comment out js array creation code.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,16 @@
 let gridSide = 16;
 let sideLength = 10;
-let idArr = [];
+// let idArr = [];
 
 const container = document.querySelector("#container");
     container.style.maxWidth = (sideLength * gridSide + gridSide - 1) + "px";
 for (let i = 1; i <= gridSide; i++) {
     for (let j = 1; j <= gridSide; j++) {
-        //create unique id
-        idArr.push({ boxId: i + "-" + j, color: "lightgray" });
         // create grid
         const square = document.createElement("div");
-            square.id = i + "-" + j;
+            // create unique id
+            // square.id = i + "-" + j;
+            // idArr.push({ boxId: i + "-" + j, color: "lightgray" });
             square.classList.add("small-square");
             square.style.width = sideLength + "px";
             square.style.height = sideLength + "px";
