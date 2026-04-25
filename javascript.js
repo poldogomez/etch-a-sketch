36cbb64ee83dc39de9183cd5883e19fc80b43a51
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2026-04-24 21:23:56 -0500

    Add eventlistener to change div background color.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 let gridSide = 16;
 let sideLength = 10;
-// let idArr = [];
 
 const container = document.querySelector("#container");
     container.style.maxWidth = (sideLength * gridSide + gridSide - 1) + "px";
@@ -8,12 +7,17 @@ for (let i = 1; i <= gridSide; i++) {
     for (let j = 1; j <= gridSide; j++) {
         // create grid
         const square = document.createElement("div");
-            // create unique id
-            // square.id = i + "-" + j;
-            // idArr.push({ boxId: i + "-" + j, color: "lightgray" });
             square.classList.add("small-square");
             square.style.width = sideLength + "px";
             square.style.height = sideLength + "px";
         container.appendChild(square);
     };
 };
+
+const squares = document.querySelectorAll(".small-square");
+
+squares.forEach(square => {
+    square.addEventListener('mouseout', () => {
+        square.style.backgroundColor = 'blue';
+    });
+});
