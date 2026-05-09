87d2d7a9809cc7859774c631d7a41c2fbb5482df
Leopoldo Gomez III <69486922+poldogomez@users.noreply.github.com>
2026-05-08 20:51:00 -0500

    Add grid size button to interface.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
 let gridSide = 32;
 let sideLength = 10;
 
+const titleElement = document.querySelector('h1');
+const btnDiv = document.createElement('div');
+    titleElement.appendChild(btnDiv);   
+
+const gridSizeBtn = document.createElement('button');
+    gridSizeBtn.textContent = "Set new grid size";
+    btnDiv.appendChild(gridSizeBtn);
+
 const container = document.querySelector("#container");
+    // set max width
     container.style.maxWidth = (sideLength * gridSide + gridSide - 1) + "px";
 for (let i = 1; i <= gridSide; i++) {
     for (let j = 1; j <= gridSide; j++) {
